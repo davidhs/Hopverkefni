8f7b232aab0e90b299281b54ff86d5d48a712ada
dingolfsson <dai5@hi.is>
2017-10-29 18:06:05 +0000

    Minor changes

--- poc1/js/Bullet.js
+++ poc1/js/Bullet.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // A generic contructor which accepts an arbitrary descriptor object
-function Bullet(descr){
+function Bullet(descr) {
 
     // Common inherited setup logic from Entity
     this.setup(descr);
@@ -21,7 +21,7 @@ Bullet.prototype.fireSound = new Audio(
     "audio/bulletFire.ogg");
 Bullet.prototype.zappedSound = new Audio(
     "audio/bulletZapped.ogg");
-    
+
 // Initial, inheritable, default values
 Bullet.prototype.rotation = 0;
 Bullet.prototype.cx = 200;
@@ -37,7 +37,7 @@ Bullet.prototype.update = function (du) {
     // TODO: YOUR STUFF HERE! --- Unregister and check for death
     spatialManager.unregister(this);
 
-    
+
     if (!g_world.inBounds(this.cx, this.cy)) return entityManager.KILL_ME_NOW;
 
     this.lifeSpan -= du;
@@ -58,7 +58,7 @@ Bullet.prototype.update = function (du) {
 
         return entityManager.KILL_ME_NOW;
     }
-    
+
     // TODO: YOUR STUFF HERE! --- (Re-)Register
     spatialManager.register(this);
 
