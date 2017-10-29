de25b632df9ed405101f6df049670a1ab967cbaf
dingolfsson <dai5@hi.is>
2017-10-29 18:20:37 +0000

    Prettier Viewport

--- poc1/js/g_viewport.js
+++ poc1/js/g_viewport.js
@@ -1,45 +1,36 @@
 "use strict";
 
-var g_viewport = (function () {
+var g_viewport = (function() {
+	var viewport = {};
 
-    var viewport = {};
+	viewport.canvas = document.createElement("canvas");
+	viewport.ctx = viewport.canvas.getContext("2d");
 
+	var _world = null;
 
+	viewport.cx = 500;
+	viewport.cy = 500;
 
+	viewport.width = 600;
+	viewport.height = 0;
 
-    viewport.canvas = document.createElement('canvas');
-    viewport.ctx = viewport.canvas.getContext('2d');
+	var width = 0;
+	var height = 0;
 
-    var _world = null;
+	var _canvas = null;
 
+	function setWorld(world) {
+		_world = world;
+	}
 
-    viewport.cx = 500;
-    viewport.cy = 500;
+	function setCanvas(canvas) {
+		_canvas = canvas;
+	}
 
+	viewport.update = function() {};
 
-    viewport.width = 600;
-    viewport.height = 0;
+	viewport.setWorld = setWorld;
+	viewport.setCanvas = setCanvas;
 
-    var width = 0;
-    var height = 0;
-
-    var _canvas = null;
-
-    function setWorld(world) {
-        _world = world;
-    }
-
-    function setCanvas(canvas) {
-        _canvas = canvas;
-    }
-
-    viewport.update = function () {
-
-    };
-
-    viewport.setWorld = setWorld;
-    viewport.setCanvas = setCanvas
-
-    return viewport;
+	return viewport;
 })();
-
