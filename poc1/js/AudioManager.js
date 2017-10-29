5693a6a2a677c0bb951720194e16b7be4d41ec9e
dingolfsson <dai5@hi.is>
2017-10-29 18:15:12 +0000

    Prettier AudioManager

--- poc1/js/AudioManager.js
+++ poc1/js/AudioManager.js
@@ -2,55 +2,48 @@
 
 // TODO this crap is useless
 
-var audioManager = (function () {
+var audioManager = (function() {
+	let assets = {};
 
+	function play(url) {
+		if (!assets[url]) {
+			add([
+				{
+					url: url,
+					volume: 0.5
+				}
+			]);
+		}
 
-    let assets = {};
+		let audio = assets[url].audio;
 
+		audio.currentTime = 0;
+		audio.play();
+	}
 
+	function add(arr) {
+		for (let i = 0; i < arr.length; i++) {
+			let obj = arr[i];
 
+			let url = obj.url;
 
-    function play(url) {
+			if (assets[url]) continue;
 
-        if (!assets[url]) {
-            add([{
-                url: url,
-                volume: 0.5
-            }]);
-        }
+			let volume = obj.volume;
 
-        let audio = assets[url].audio;
+			let audio = new Audio(url);
+			audio.volume = volume;
 
-        audio.currentTime = 0;
-        audio.play();
-    }
+			assets[url] = {
+				url: url,
+				volume: volume,
+				audio: audio
+			};
+		}
+	}
 
-    function add(arr) {
-
-        for (let i = 0; i < arr.length; i++) {
-
-            let obj = arr[i];
-
-            let url = obj.url;
-
-            if (assets[url]) continue;
-
-            let volume = obj.volume;
-
-            let audio = new Audio(url);
-            audio.volume = volume;
-
-            assets[url] = {
-                url: url,
-                volume: volume,
-                audio: audio
-            };
-        }
-    }
-
-
-    return {
-        add: add,
-        play: play
-    };
+	return {
+		add: add,
+		play: play
+	};
 })();
