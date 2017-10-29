c805b2c5148b0e199293d86ffb9a7693e9fab1fb
dingolfsson <dai5@hi.is>
2017-10-29 18:17:34 +0000

    Prettier consts

--- poc1/js/consts.js
+++ poc1/js/consts.js
@@ -5,8 +5,8 @@
 "use strict";
 
 var consts = {
-    TAU: 2 * Math.PI,
-    FULL_CIRCLE: Math.PI * 2,
-    RADIANS_PER_DEGREE: Math.PI / 180.0,
-    RADIANS_PER_DEGREE: 180.0 / Math.PI
+	TAU: 2 * Math.PI,
+	FULL_CIRCLE: Math.PI * 2,
+	RADIANS_PER_DEGREE: Math.PI / 180.0,
+	RADIANS_PER_DEGREE: 180.0 / Math.PI
 };
