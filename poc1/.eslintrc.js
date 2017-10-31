d25e9bb752137afc1403eb3ab6b86dbb0cf5244c
dingolfsson <dai5@hi.is>
2017-10-31 01:34:14 +0000

    Eslint Rules Changed

--- poc1/.eslintrc.js
+++ poc1/.eslintrc.js
@@ -2,7 +2,11 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "no-unused-vars": [0, { "vars": "local" }],
+        "no-undef": [0, { "vars": "local" }],
+        "no-underscore-dangle": [0, { "vars": "local" }],
         "func-names": ["error", "never"],
+        "camelcase": [0, "global"],
+        "no-plusplus": ["error", { "allowForLoopAfterthoughts": true }],
         "strict": [0, "global"]
     }
 };
