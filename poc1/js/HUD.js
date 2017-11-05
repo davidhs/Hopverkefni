21da1db7075048d605b48b0034b10da582167c51
alli959 <alg35@hi.is>
2017-11-05 21:14:56 +0000

    er ekki að átta mig á hvernig ég tek við myndum sem ég set í json skrána, hér er allavegana hugmynd hvernig ég vil hafa þetta, ætla að halda áfram að reyna að átta mig á þessu

--- poc1/js/HUD.js
+++ poc1/js/HUD.js
@@ -4,6 +4,27 @@
 
   const HUD = (function () {
   const HUD = {};
+
+
+  //globals for health bar
+  HUD.xHP = 30;
+  HUD.yHP = 10;
+  HUD.hpLost = 0;
+
+  //Keycodes
+  HUD.KEY_RIFLE = '1'.charCodeAt(0);
+  HUD.KEY_HANDGUN = '2'.charCodeAt(0);
+  HUD.KEY_SHOTGUN = '3'.charCodeAt(0);
+  HUD.KEY_KNIFE = '5'.charCodeAt(0);
+
+
+  //toggle on or off weapon images
+  HUD.isRifle = true;
+  HUD.isHandgun = false;
+  HUD.isShotgun = false;
+  HUD.isKnife = false;
+
+
   HUD.canvas = document.getElementById('hudCanvas');
   HUD.ctx = HUD.canvas.getContext('2d');
   HUD.ctx.beginPath();
@@ -12,14 +33,57 @@
   HUD.ctx.fill();
 
 
-  //globals for health bar
-  var xHP = 30;
-  var yHP = 10;
+  //create images
+  HUD.heart = new Image();
+  HUD.knife = new Image();
+  HUD.shotgun = new Image();
+  HUD.rifle = new Image();
+  HUD.handgun = new Image();
+
+
+
+
+  //witch weapon should appear on the screen, later
+  //witch weapon do you choose
+  HUD.witchweapon = function(){
+    if(eatKey(this.KEY_HANDGUN)){
+      HUD.isRifle = false;
+      HUD.isHandgun = true;
+      HUD.isShotgun = false;
+      HUD.isKnife = false;
+    }
+
+    if(eatKey(this.KEY_RIFLE)){
+    HUD.isRifle = true;
+    HUD.isHandgun = false;
+    HUD.isShotgun = false;
+    HUD.isKnife = false;
+
+  }
+  if(eatKey(this.KEY_SHOTGUN)){
+    HUD.isRifle = false;
+    HUD.isHandgun = false;
+    HUD.isShotgun = true;
+    HUD.isKnife = false;
+  }
+  if(eatKey(this.KEY_KNIFE)){
+    HUD.isRifle = false;
+    HUD.isHandgun = false;
+    HUD.isShotgun = true;
+    HUD.isKnife = false;
+  }
+
+
+
+
 
   //draw heart in front of health bar
-  var heart = document.getElementById("heart");
+  HUD.heart.addEventListener('load', function() {
   HUD.ctx.beginPath();
-  HUD.ctx.drawImage(heart,10,10,15,15);
+  HUD.ctx.drawImage(HUD.heart,10,10,15,15);
+  }, false);
+  HUD.heart.src = 'img/hp.png'; // Set source path
+
 
 
   function draw_healthbar(x, y, perclost, width, thickness){
@@ -29,7 +93,38 @@
     HUD.ctx.fill();
   }
 
-  draw_healthbar(xHP, yHP, 0,100, 15);
+
+  //when you lose hp you call this, but
+  //perclost goes higher depends on how much
+  //hp you lose
+  draw_healthbar(HUD.xHP, HUD.yHP, 0,100, 15);
+
+
+  //draw the defult gun, as for now,
+
+  HUD.rifle.addEventListener('load', function(){
+    HUD.ctx.beginPath();
+    HUD.ctx.drawImage(HUD.rifle, 280,40,100,30);
+  }, false);
+  HUD.rifle.src = 'img/guns/rifle.png'; //set source path
+
+
+
+  HUD.update = function(du){
+
+    //if you are hit
+
+    //if hit then call
+    //(draw_healthbar(HUD.xHP, HUD.yHP, hpLost++,100, 15))
+
+
+    //if you press buttons 1 to 5, you change weapon
+    //and another weapon appears on the screen
+
+  }
+
+
+
 
 
 
