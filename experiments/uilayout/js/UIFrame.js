a2000ede440f66991381e69da48dc4dc583aaf63
alli959 <alg35@hi.is>
2017-11-12 17:52:04 +0000

    ....

---
+++ experiments/uilayout/js/UIFrame.js
@@ -0,0 +1,60 @@
+
+
+
+function UIFrame(canvas) {
+    this.setup(this);
+    this.setPosition(0, 0);
+    this.setDimensions(canvas.width, canvas.height);
+
+    this.name = name;
+
+    this._cards = [];
+}
+
+UIFrame.prototype = Object.create(UIElement.prototype);
+
+//UIFrame.prototype = new UIElement();
+
+
+
+UIFrame.prototype.render = function (ctx) {
+
+    const oldfillStyle = ctx.fillStyle;
+
+    const x = this.getOuterX();
+    const y = this.getOuterY();
+    const w = this.getWidth();
+    const h = this.getHeight();
+
+    const backgroundColor = this.backgroundColor;
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(x, y, w, h);
+    ctx.fill();
+    ctx.fillStyle = oldfillStyle;
+
+
+    if (this.layout === this.LAYOUT_DEFAULT) {
+        this._renderChildren(ctx);
+    } else if (this.layout === this.LAYOUT_CARD) {
+        this._cards[this.cardSelection].render(ctx);
+    }
+};
+
+UIFrame.prototype.addChild = function (child, cardID) {
+
+    if (child === this) throw Error();
+    child.setParent(this);
+    this._children.push(child);
+
+    if (this.layout === this.LAYOUT_DEFAULT) {
+    } else if (this.layout === this.LAYOUT_CARD) {
+        this._cards[cardID] = child;
+    }
+
+    this.update();
+};
+
+
+UIFrame.prototype.selectCard = function (card) {
+    this.cardSelection = card;
+};
---
+++ poc1/js/lib/QuickList2.js
@@ -0,0 +1,64 @@
+'use strict';
+
+function QuickList() {
+  this._list = [];
+  this._size = 0;
+}
+
+
+QuickList.prototype.add = function (x) {
+  if (!this.has(x)) {
+    this._list[this._size] = x;
+    this._size += 1;
+    return true;
+  }
+
+  return false;
+};
+
+QuickList.prototype._exch = function (idx1, idx2) {
+  const temp = this._list[idx1];
+  this._list[idx1] = this._list[idx2];
+  this._list[idx2] = temp;
+};
+
+// true if removed, false if not
+QuickList.prototype.remove = function (x) {
+  // Search for value
+  for (let i = 0; i < this._size; i += 1) {
+    const y = this._list[i];
+    if (x === y) {
+      this._exch(i, this._size - 1);
+      if (true) {
+        delete this._list[this._size - 1];
+        // this._list.splice(this._size - 1, 1);
+      }
+      this._size -= 1;
+      return true;
+    }
+  }
+
+  return false;
+};
+
+
+QuickList.prototype.has = function (x) {
+  // Search for value
+  for (let i = 0; i < this._size; i += 1) {
+    const y = this._list[i];
+    if (x === y) {
+      // MAYBE BAD IDEA
+      this._exch(0, i);
+      return true;
+    }
+  }
+  return false;
+};
+
+QuickList.prototype.getList = function () {
+  return this._list;
+};
+
+QuickList.prototype.getSize = function () {
+  return this._size;
+};
